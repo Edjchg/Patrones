20658722e2892d0916e59670cf1a34beaffd9e7a
edgarchaves <edjchg@gmail.com>
2018-03-22 22:30:30 -0600

    AbstractFactory

---
+++ AbstractFactory.h
@@ -0,0 +1,14 @@
+//
+// Created by Edgar Chaves on 22/3/18.
+//
+
+#ifndef PATRONES_ABSTRACTFACTORY_H
+#define PATRONES_ABSTRACTFACTORY_H
+
+
+class AbstractFactory {
+
+};
+
+
+#endif //PATRONES_ABSTRACTFACTORY_H
